2aac892193af9b9ff332cf2fb2f897926588d17e
David Krammer <david.krammer@protonmail.com>
2022-09-03 13:45:17 +0200

    Initial

---
+++ Components/MyModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SelectBug.Components;
+
+public class MyModel
+{
+    public MyModel(Guid id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+}
